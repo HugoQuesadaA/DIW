e9df7c0d8237d3b6893c271a261f86f524a2f3cc
HugoQuesadaA <hugoquesalv@gmail.com>
2024-11-20 21:44:56 +0100

    gifs y animaciones

---
+++ Temas/Tema5/gifs-animaciones/ani-js.js
@@ -0,0 +1,31 @@
+function translateCaja(){
+    const caja = document.querySelector('.caja');
+    caja.classList.toggle('caja-trans');
+
+    const img = document.querySelector('.img');
+    img.classList.toggle('img-trans');
+}
+
+function rotate2DCaja(){
+    const caja = document.querySelector('.caja');
+    caja.classList.toggle('caja-rot2d');
+
+    const img = document.querySelector('.img');
+    img.classList.toggle('img-rot2d');
+}
+
+function rotate3dCaja(){
+    const caja = document.querySelector('.caja');
+    caja.classList.toggle('caja-rot3d');
+
+    const img = document.querySelector('.img');
+    img.classList.toggle('img-rot3d');
+}
+
+function skewCaja(){
+    const caja = document.querySelector('.caja');
+    caja.classList.toggle('caja-skew');
+
+    const img = document.querySelector('.img');
+    img.classList.toggle('img-skew');
+}
