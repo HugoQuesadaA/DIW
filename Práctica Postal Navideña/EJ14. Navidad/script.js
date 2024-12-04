08e367936ac7e5af51107871c6244724104c1da4
HugoQuesadaA <hugoquesalv@gmail.com>
2024-12-04 21:37:15 +0100

    practicaNavidad

---
+++ Práctica Postal Navideña/EJ14. Navidad/script.js
@@ -0,0 +1,7 @@
+// El archivo js...
+// Así os ahorro el gran trabajo y sudor que supone crearlo y así Miguel está obligado a no integrarlo en el HTML
+function elem_visibles(){
+    const bolanieve = document.querySelector('.bolanieve');
+    bolanieve.classList.toggle('bolanieveTranslate');
+}
+
