3fb8c713acfbe493dc23bc5731026d32c82f2e87
HugoQuesadaA <hugoquesalv@gmail.com>
2024-12-10 19:57:02 +0100

    hog 2

---
+++ Temas/Tema5/hogGame/script.js
@@ -0,0 +1,13 @@
+function intento(objeto){
+
+}
+
+function ordenAleatorio(){
+    const copia = [...objetosIniciales];
+
+    for(let i = copia.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+
+        [copia[i]]
+    }
+}
