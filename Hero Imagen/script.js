f6e442927faf71080d09bd586abcba8c5626b816
HugoQuesadaA <hugoquesalv@gmail.com>
2025-02-10 18:36:14 +0100

    HERO COMMIT FINAL

--- Hero Imagen/script.js
+++ Hero Imagen/script.js
@@ -1,3 +1,17 @@
+moverTexto();
+moverImagenes();
+animacionTitulo();
+
+function animacion(n, b) { 
+    eliminarClases();
+    moverFondo(n, b);
+    setTimeout(() => {
+        moverTexto();
+        moverImagenes();
+        animacionTitulo();
+    }, 1000);
+}
+
 function moverFondo(n, b) {
     let fondos = document.querySelectorAll('.fondo');
     let botones = document.querySelectorAll('.boton');
@@ -11,4 +25,50 @@ function moverFondo(n, b) {
     });
 
     b.classList.add("buttonWhite");
+}
+
+function moverTexto() {
+    let textos = document.querySelectorAll('.texto');
+
+    textos.forEach(texto => {
+        texto.classList.add("animacionTexto");
+    });
+}
+
+function moverImagenes() {
+    let imagenes = document.querySelectorAll('.imagen');
+
+    imagenes.forEach(imagen => {
+        imagen.classList.add("animacionImagen");
+    });
+}
+
+function animacionTitulo(){
+    let titulos = document.querySelectorAll('.h1');
+
+    titulos.forEach(titulo => {
+        titulo.classList.add("animacionTitulo");
+    });
+}
+
+function eliminarClases() {
+
+    let textos = document.querySelectorAll('.texto');
+
+    textos.forEach(texto => {
+        texto.classList.remove("animacionTexto");
+        console.log('Clase animacionTexto eliminada de:', texto);
+    });
+
+    let imagenes = document.querySelectorAll('.imagen');
+
+    imagenes.forEach(imagen => {
+        imagen.classList.remove("animacionImagen");
+    });
+    
+    let titulos = document.querySelectorAll('.h1');
+
+    titulos.forEach(titulo => {
+        titulo.classList.remove("animacionTitulo");
+    });
 }
